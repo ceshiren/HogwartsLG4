2bc153f502f71db67e49c54ed1eb1a3373472969
seveniruby <seveniruby@gmail.com>
2020-10-27 21:45:10 +0800

    pytest测试用例入门

--- test_pytest/tests/test_calc.py
+++ test_pytest/tests/test_calc.py
@@ -19,3 +19,29 @@ class TestCalc:
         assert self.calc.mul(a, b) == c
         # assert calc.mul(-1, -1) == 1
         # assert calc.mul(1, -1) == 1
+
+    # 正常值例子
+    @pytest.mark.parametrize('a, b, c', [
+        [2, 2, 1],
+        [0.2, 0.1, 2],
+        [0, 2, 0]
+    ])
+    def test_div(self, a, b, c):
+        assert self.calc.div(a, b) == c
+
+    # 异常值例子
+    @pytest.mark.parametrize('a, b', [
+        [2, 0],
+        [0.2, 0],
+        [0, 0]
+    ])
+    def test_div(self, a, b):
+        with pytest.raises(Exception):
+            assert self.calc.div(a, b)
+
+    # 流程示例
+    def test_process(self):
+        r1=self.calc.mul(1, 2)
+        r2=self.calc.div(2, 1)
+        assert r1 == 2
+        assert r2 == 2
