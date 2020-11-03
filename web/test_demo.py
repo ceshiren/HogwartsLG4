fc343434072ae79923d33bf4fba51f877e4a462c
xj418974188 <xujuan418974188@163.com>
2020-11-03 20:42:49 +0800

    web 自动化测试 selenium IDE录制脚本改造

---
+++ web/test_demo.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from time import sleep
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+class TestTestcase():
+    def setup_method(self, method):
+        self.driver = webdriver.Chrome()
+        # 隐式等待
+        self.driver.implicitly_wait(5)
+
+    def teardown_method(self, method):
+        self.driver.quit()
+
+    def test_testcase(self):
+        self.driver.get("https://ceshiren.com/")
+        self.driver.find_element(By.LINK_TEXT, "所有分类").click()
+        element = self.driver.find_element(By.LINK_TEXT, "所有分类")
+        result = element.get_attribute("class")
+        assert 'active' == result
